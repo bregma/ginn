57a07ceb8dd46635349249d5a88eb6c4525804ad
Mohamed-ikbel Boulabiar <boulabiar@gmail.com>
2010-11-03 15:33:54 +0100

    Split the wishes parsing function for clarity.

--- src/config.c
+++ src/config.c
@@ -68,71 +68,75 @@ static int ginn_str2bool(const char *str){
 	return -2;
 }
 
+void store_1config(xmlNode *node, struct wish *wp, int *position)
+{
+     if (0==strcmp(node->name, "wish")) {
+	// printf(" gesture %s fingers %s ",(xmlGetProp(node, "gesture")),(xmlGetProp(node, "fingers")));
+	wp->config_attr[0].attrName = "gesture name";
+	switch (xmlGetProp(node, "gesture")[0]) {
+		case 'D': wp->config_attr[0].val=wp->config_attr[0].valMax= 0; break; 
+		case 'P': wp->config_attr[0].val=wp->config_attr[0].valMax= 1; break;  
+		case 'R': wp->config_attr[0].val=wp->config_attr[0].valMax= 2; break;  
+		case 'T': wp->config_attr[0].val=wp->config_attr[0].valMax=15; break;  
+	}
+	wp->config_attr[1].attrName = "touches";
+	wp->config_attr[1].val      = atoi(xmlGetProp(node, "fingers"));
+	wp->config_attr[1].valMax   = atoi(xmlGetProp(node, "fingers"));
+     }
+     if (0==strcmp(node->name, "action")) {
+	     wp->when = ginn_str2state(xmlGetProp(node, "when"));
+	     if (wp->when < 0)
+		     fprintf(stderr, "ERROR: you must provide property 'when' to the action: %s\n", xmlGetProp(node, "name"));
+     }
+     if (0==strcmp(node->name, "trigger")) {
+	wp->config_attr[*position].attrName = xmlGetProp(node, "prop");
+	wp->config_attr[*position].val = atoi(xmlGetProp(node, "min"));
+	wp->config_attr[*position].valMax = atoi(xmlGetProp(node, "max"));
+	int acc = ginn_str2bool(xmlGetProp(node, "accumulate"));
+	wp->config_attr[*position].accumulate = acc == -1 ? GINN_DEFAULT_ACCUMULATE : acc;
+	wp->config_attr[*position].accumVal = 0;
+	(*position)++;
+     }
+     if (0==strcmp(node->name, "key")) {
+	wp->key = xmlNodeGetContent(node);
+	if (xmlGetProp(node, "modifier1"))
+		wp->modifiers[0] = xmlGetProp(node, "modifier1"); 
+	if (xmlGetProp(node, "modifier2"))
+		wp->modifiers[1] = xmlGetProp(node, "modifier2"); 
+	if (xmlGetProp(node, "modifier3"))
+		wp->modifiers[2] = xmlGetProp(node, "modifier3"); 
+	if (xmlGetProp(node, "modifier4"))
+		wp->modifiers[3] = xmlGetProp(node, "modifier4");
+     }
+}
+
 void parse_node(const xmlNode *root, int depth, struct wish *wp)
 {
 	xmlNode *node, *tnode;
 	struct wish *wpNext, *topwp;
 	int position=2;
-	topwp=wp;	
+	topwp=wp;
+		
 	for (node = root; node; node = node->next) {
 		do {
 			if (node->type != XML_ELEMENT_NODE)
 				continue;
-		     if (0==strcmp(node->name, "wish")) {
-		  	printf(" gesture %s fingers %s ",
-				(xmlGetProp(node, "gesture")),
-				(xmlGetProp(node, "fingers")));
-		  	wp->config_attr[0].attrName = "gesture name";
-			switch (xmlGetProp(node, "gesture")[0]) {
-				case 'D': wp->config_attr[0].val=wp->config_attr[0].valMax= 0; break; 
-				case 'P': wp->config_attr[0].val=wp->config_attr[0].valMax= 1; break;  
-				case 'R': wp->config_attr[0].val=wp->config_attr[0].valMax= 2; break;  
-				case 'T': wp->config_attr[0].val=wp->config_attr[0].valMax=15; break;  
-			}
-			wp->config_attr[1].attrName = "touches";
-		  	wp->config_attr[1].val      = atoi(xmlGetProp(node, "fingers"));
-		  	wp->config_attr[1].valMax   = atoi(xmlGetProp(node, "fingers"));
-		     }
-		     if (0==strcmp(node->name, "action")) {
-			     wp->when = ginn_str2state(xmlGetProp(node, "when"));
-			     if (wp->when < 0)
-				     fprintf(stderr, "ERROR: you must provide property 'when' to the action: %s\n", xmlGetProp(node, "name"));
-		     }
-		     while (0==strcmp(node->name, "trigger")) {
-			wp->config_attr[position].attrName = xmlGetProp(node, "prop");
-		  	wp->config_attr[position].val = atoi(xmlGetProp(node, "min"));
-		  	wp->config_attr[position].valMax = atoi(xmlGetProp(node, "max"));
-		  	int acc = ginn_str2bool(xmlGetProp(node, "accumulate"));
-			wp->config_attr[position].accumulate = acc == -1 ? GINN_DEFAULT_ACCUMULATE : acc;
-			wp->config_attr[position].accumVal = 0;
-			position++;
-			if (node->next) 
-				node = (node->next);
-		     }
+			store_1config(node, wp, &position);
+
 		     if (0==strcmp(node->name, "key")) {
-			wp->key = xmlNodeGetContent(node);
-			if (xmlGetProp(node, "modifier1"))
-				wp->modifiers[0] = xmlGetProp(node, "modifier1"); 
-			if (xmlGetProp(node, "modifier2"))
-				wp->modifiers[1] = xmlGetProp(node, "modifier2"); 
-			if (xmlGetProp(node, "modifier3"))
-				wp->modifiers[2] = xmlGetProp(node, "modifier3"); 
-			if (xmlGetProp(node, "modifier4"))
-				wp->modifiers[3] = xmlGetProp(node, "modifier4"); 
 			if (node->parent->parent->next){
 				node = (node->parent->parent->next);
 				position=2;
-				//FIXME: There is always an extra allocation (i.e: 4 allocation for 3 wishes)
 				wpNext = (struct wish *) malloc(sizeof(struct wish));
 				init(wpNext);
 				wp = wp->next = wpNext;
 			}
 		     }
 		} while (node->children && (node = node->children));
+		//printf("\nIKBEL Apps %s", root->children->next->next->next->name);
 	}
 	wp=topwp;
 }
-
 
 void ginn_config_store(const struct ginn_config *cfg, struct wish *w)
 {
